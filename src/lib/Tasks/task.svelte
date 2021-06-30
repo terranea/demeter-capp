e2719b007c87aeabf7e19277e8e9e13c9261cdb8
jakoblo <jakoblo@pm.me>
2021-06-30 17:40:33 +0200

    video inline

--- src/lib/Tasks/task.svelte
+++ src/lib/Tasks/task.svelte
@@ -142,7 +142,7 @@
 
       <div class="preview">
         {#if supported}
-        <video id="player" bind:this={player} autoplay controls=""/>
+        <video id="player" bind:this={player} autoplay webkit-playsinline/>
         {:else}
         {#if imageSrc}
           <img id="blah" src={imageSrc} alt="your image" />
