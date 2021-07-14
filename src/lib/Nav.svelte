043191cabd5223d93a2bdb7034f6ab93ffd37bdd
jakoblo <jakoblo@pm.me>
2021-07-14 12:57:13 +0200

    cleanup camera

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -27,11 +27,6 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map"><polygon points="1 6 1 22 8 18 16 22 23 18 23 2 16 6 8 2 1 6"></polygon><line x1="8" y1="2" x2="8" y2="18"></line><line x1="16" y1="6" x2="16" y2="22"></line></svg>
       Parcels</a>
   </div>
-  <!-- <div class="item" class:active={$page.path === "/camera"}>
-    <a sveltekit:prefetch href="/camera">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-camera"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-      Camera</a>
-  </div> -->
 </nav>
 
 <style>
--- src/routes/camera.svelte
+++
@@ -1,7 +0,0 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
-<svelte:head>
-	<title>Camera</title>
-</svelte:head>
