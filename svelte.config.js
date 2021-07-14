5c5f9068cf2552e98692055582135de10ecd4afb
jakoblo <jakoblo@pm.me>
2021-07-14 12:47:35 +0200

    netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel(),
+		adapter: adapter(),
 		vite: {
 			optimizeDeps: {
 				exclude: ['@urql/svelte', 'subscriptions-transport-ws'],
